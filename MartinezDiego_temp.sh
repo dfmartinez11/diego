714caa4de75f320df21ca9addde12019b7739ed9
dfmartinez11 <42391122+dfmartinez11@users.noreply.github.com>
2018-08-31 18:46:41 -0500

    Add files via upload

---
+++ MartinezDiego_temp.sh
@@ -0,0 +1,5 @@
+mkdir Temperaturas
+cd Temperaturas
+wget https://climate.nasa.gov/system/internal_resources/details/original/647_Global_Temperature_Data_File.txt
+cd ..
+cp tempdata.txt Temperaturas
