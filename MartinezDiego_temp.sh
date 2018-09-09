50b067af53b9912597c9d076bea000dc6d1f67a9
dfmartinez11 <42391122+dfmartinez11@users.noreply.github.com>
2018-09-09 04:06:23 -0500

    Delete MartinezDiego_temp.sh

--- MartinezDiego_temp.sh
+++
@@ -1,5 +0,0 @@
-mkdir Temperaturas
-cd Temperaturas
-wget https://climate.nasa.gov/system/internal_resources/details/original/647_Global_Temperature_Data_File.txt
-cd ..
-cp tempdata.txt Temperaturas
