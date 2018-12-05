f164ebee2a15552ea2dd07331c9106daffbe740c
Default user <df.martinez11@uniandes.edu.co>
2018-12-05 20:04:11 +0000

    here

--- graficar.py
+++ graficar.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import fft as f
 
+
+
+
 datos = np.loadtxt("datos.dat")
 x = datos.transpose()[0]
 y = datos.transpose()[1]
@@ -15,5 +18,25 @@ v1.plot(x,2*y +1, color ="g")
 v2=plt.subplot(1,2,2)
 #plt.xlim(0,15)
 v2.plot(x,y+np.random.rand(len(y)), color ="b")
-
 a.savefig("graficaCPP.png")
+
+y = y + np.random.rand(len(y))
+fo = f.fftshift(f.fft(y))
+x = f.fftshift(f.fftfreq(100))
+#y[abs(x) > 0.1] = 0
+
+fo[abs(x) > 0.06] = 0
+plt.figure()
+a1=plt.subplot(1,2,1)
+a1.plot(x,abs(fo))
+
+iy = f.ifft(fo)
+a2 = plt.subplot(1,2,2)
+a2.plot(x,iy, color="r")
+#plt.xlim(0,0.1)
+plt.savefig("fourier.png")
+
+
+                       
+                      
+                      
