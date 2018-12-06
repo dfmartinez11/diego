49ab695a7675747f40ec92a48eea88ae371f1d47
Default user <df.martinez11@uniandes.edu.co>
2018-12-06 18:56:30 +0000

    here

--- ODequation.cpp
+++ ODequation.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int N=300;
 double x[3000];
+double t[3000];
 double dt=0.01;
 
 double derivada(double x);
 void euler(double *x, double condicionI);
+void runge(double *x, double condicionI);
+void leapf(double *x, double cond);
 
 int main(){
     FILE *f;
@@ -18,6 +21,24 @@ int main(){
         fprintf(f , "%f %f \n", i*dt, x[i]);
     }
     fclose(f);
+    
+    FILE *f2;
+    f2 = fopen("difSencillaRunge.dat" ,"w");
+    runge(x , 0.0);
+    cout << "Pinche ponquesito" << endl;
+    for(int i=0 ; i<N ;i++){
+        fprintf(f2 , "%f %f \n", i*dt, x[i]);
+    }
+    fclose(f2);
+    
+    FILE *f3;
+    f3 = fopen("difSencillaLeapF.dat" ,"w");
+    leapf(x , 0.0);
+    cout << "Pinche ponquesito" << endl;
+    for(int i=0 ; i<N ;i++){
+        fprintf(f3 , "%f %f \n", i*dt, x[i]);
+    }
+    fclose(f3);
     return 0;
 }
 
@@ -27,7 +48,28 @@ double derivada(double x){
 
 void euler(double *x, double condicionI){
     x[0] = condicionI;
+    t[0] = 0.0;
     for(int i=1 ; i<N ;i++){
-        x[i] = x[i-1] + dt*( cos(x[i-1]) );
+        x[i] = x[i-1] + dt*( cos(t[i-1]) );
     }
+}
+
+void runge(double *x, double condicionI){
+    x[0] = condicionI;
+    t[0] = 0.0;
+    for(int i=1 ; i<N ;i++){
+        double k1 = dt*( cos(t[i-1]) );
+        double k2 = dt*( cos(t[i-1]) + (0.5*k1)  );
+        x[i] = x[i-1] + k2;
+    }
+}
+
+void leapf(double *x , double cond){
+    x[0] = cond;
+    t[0] = 0.0;
+    for(int i=2 ; i<N ; i++){
+        t[i] = t[i-1] + dt;
+        x[i] = x[i-2] + ( 2*dt*cos(t[i-1]) );
+    }
+    
 }
--- graficar.py
+++ graficar.py
@@ -5,20 +5,21 @@ from numpy import fft as f
 
 
 
-#datos = np.loadtxt("datos.dat")
-datos = np.loadtxt("difSencilla.dat")
+datos = np.loadtxt("datos.dat")
+dato = np.loadtxt("difSencilla.dat")
+datoss = np.loadtxt("difSencillaLeapF.dat")
 
 x = datos.transpose()[0]
 y = datos.transpose()[1]
 
 a=plt.figure()
-v1 = plt.subplot(2,2,1)
+v1 = plt.subplot(3,2,1)
 #plt.xlim(0,10)
 v1.plot(x,y, color="r")
 v1.plot(x,2*y +1, color ="g")
 plt.legend(["original" , "aumentada"])
 
-v2=plt.subplot(2,2,2)
+v2=plt.subplot(3,2,2)
 #plt.xlim(0,15)
 v2.plot(x,y+np.random.rand(len(y)), color ="b")
 
@@ -30,15 +31,31 @@ x = f.fftshift(f.fftfreq( len(y) ))
 
 fo[abs(x) > 0.0085] = 0
 
-a1=plt.subplot(2,2,3)
+a1=plt.subplot(3,2,3)
 a1.plot(x,abs(fo))
 plt.legend(["fourier ODE"])
 
 iy = f.ifft(fo)
-a2 = plt.subplot(2,2,4)
+a2 = plt.subplot(3,2,4)
 a2.plot(x,iy, color="r")
 #plt.xlim(0,0.1)
 plt.legend(["inversa"])
+
+
+dato = np.loadtxt("difSencillaRunge.dat")
+
+v3 = plt.subplot(3,2,5)
+v3.plot(dato.transpose()[0],dato.transpose()[1], color="b")
+v3.plot(datos.transpose()[0],datos.transpose()[1], color="r")
+#plt.xlim(0,0.1)
+plt.legend(["Runge","Euler"])
+
+v4 = plt.subplot(3,2,6)
+v4.plot(dato.transpose()[0],dato.transpose()[1], color="b")
+v4.plot(datoss.transpose()[0],datoss.transpose()[1], color="g")
+#plt.xlim(0,0.1)
+plt.legend(["Runge","LeapF"])
+
 
 a.savefig("graficaCPP.png")
 
