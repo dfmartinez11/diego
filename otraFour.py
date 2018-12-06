570de1b4742f339de8938db754ab3bef979fc251
Default user <df.martinez11@uniandes.edu.co>
2018-12-06 00:31:56 +0000

    here

--- otraFour.py
+++ otraFour.py
@@ -1,6 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import fft as f
+from scipy import interpolate as interp
+
+x1 = np.linspace(0,6,10)
+y1 = np.sin(x1)
+plt.figure()
+v1 = plt.subplot(2,1,1)
+v1.scatter(x1,y1)
+y =  interp.interp1d(x1,y1)
+x= np.linspace(0,6,1000)
+v1.plot(x,y(x), c="g")
+plt.legend(["datos", "lineal"])
+
+v2 = plt.subplot(2,1,2)
+v2.scatter(x1,y1)
+y =  interp.interp1d(x1,y1, kind = "cubic")
+x = np.linspace(0,6,1000)
+v2.plot(x,y(x), c="r")
+plt.legend(["datos", "cubica"])
+plt.savefig("interp.png")
+
+
 
 n=100
 x= np.linspace(0,2*3.14,n)
@@ -22,7 +43,7 @@ yo[abs(xo) > 0.01] = 0
 
 v3 =plt.subplot(4,1,3)
 plt.xlim(-1.0,1.0)
-v3.plot(xo[delta],abs(yo[delta]),label="filtradaFourier")
+#v3.plot(xo[delta],abs(yo[delta]),label="filtradaFourier")
 v3.plot(xo,abs(yo),label="filtradaFourier")
 plt.legend()
 
